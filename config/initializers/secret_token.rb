b86d0a0c27d3bfbe942fb4155543aef267ff337e
Seiei Higa <hanachin@gmail.com>
2014-01-07 19:35:13 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuntun::Application.config.secret_key_base = 'f93b083d5cefc532b290369d91a508c336096fce25e9b15d8137770a1ff3cf729b7e73591818f2ed488d907d1da770677ff7e56cad508b18831b5df9814d7053'
