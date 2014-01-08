0f5d06c36c366eebcbb6991451a673dc8917160f
Seiei Higa <hanachin@gmail.com>
2014-01-08 15:03:25 +0900

    grunt connect proxyの設定に合わせて/api以下でresourceを定義するようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Tuntun::Application.routes.draw do
-  resources :websites
+  scope '/api' do
+    resources :websites
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
